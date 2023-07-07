5998b16771331300d77443c236eb4e28339006c0
Rafael Petouris Rodriguez de Paterna <R.PetourisRodriguezdePaterna@student.tudelft.nl>
2023-07-07 17:09:39 +0200

    Initial Skeleton Frame
    Added Initial Skeleton Frame

--- SigPAC-Extractor/src/Main.java
+++ SigPAC-Extractor/src/Main.java
@@ -1,5 +1,73 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
+    static Scanner inputScanner;
     public static void main(String[] args) {
-        System.out.println("SigPAC Extractor");
+        System.out.println("SigPAC ToolKit by Rafael Petouris");
+        inputScanner = new Scanner(System.in);
+
+        String option = "";
+
+        while (!option.equals("3")) {
+            System.out.println(
+                    "-------------------------------- \n" +
+                            "Por favor, indique el número de la opción a realizar: \n" +
+                            "1 - Escanear pdf a fichero \n" +
+                            "2 - Comparar diferencias entre ficheros \n" +
+                            "3 - Cerrar aplicación \n" +
+                            "-------------------------------- "
+            );
+
+            option = inputScanner.nextLine();
+
+            optionSwitchCase(option);
+        }
+
+
+        inputScanner.close();
+
+    }
+
+    private static void optionSwitchCase(String option) {
+        switch (option) {
+            case "1":
+                System.out.println("Ha elegido 'Escanear pdf a fichero:'");
+                pdfExtractor();
+                break;
+            case "2":
+                System.out.println("Ha elegido 'Comparar diferencias entre ficheros'");
+                break;
+            case "3":
+                System.out.println("Muchas gracias por usar este servicio.\n" +
+                        "Si ha tenido problemas, por favor contacte con r.petouris@gmail.com");
+                break;
+            default:
+                System.out.println("El número introducido no es válido. Inténtelo de nuevo \n");
+                break;
+        }
+    }
+
+    private static void pdfExtractor() {
+        System.out.println("SigPAC Extractor by Rafael Petouris");
+
+        String fileName = fileNameInput();
+        System.out.println(fileName);
+    }
+
+
+    /**
+     * Method to scan fileName from user input;
+     *
+     * @return String fileName
+     */
+    private static String fileNameInput() {
+
+        System.out.print("Por favor, escriba el nombre del pdf a leer: \n");
+        String fileName = inputScanner.nextLine();
+
+        return fileName;
     }
 }
