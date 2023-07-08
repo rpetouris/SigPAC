53cc4dd953079fb6b6eb17d7cb388239e231ee85
Rafael Petouris Rodriguez de Paterna <R.PetourisRodriguezdePaterna@student.tudelft.nl>
2023-07-08 10:19:23 +0200

    Importing Optimizing
    Starting from now Ill start branching

--- SigPAC-Extractor/src/Classes/Extractor.java
+++ SigPAC-Extractor/src/Classes/Extractor.java
@@ -1,10 +1,6 @@
 package Classes;
 
-import java.util.Scanner;
-
-import static Classes.Shared.fileNameInput;
-
-public class Extractor{
+public class Extractor {
 
     private static Shared shared;
 
@@ -15,13 +11,13 @@ public class Extractor{
         System.out.println("SigPAC Extractor por Rafael Petouris");
 
         String pdfFileName = shared.fileNameInput();
-        System.out.println("Ha decidido abrir el archivo pdf \"" + pdfFileName +"\"");
+        System.out.println("Ha decidido abrir el archivo pdf \"" + pdfFileName + "\"");
 
         //Fill in extraction function
 
         System.out.println("¿Cómo quiere llamar a esta tabla?");
         String tableName = shared.getInputScanner().nextLine();
 
-        System.out.println("La tabla se ha guardado como \"" + tableName +"\""); //change to path after implementing
+        System.out.println("La tabla se ha guardado como \"" + tableName + "\""); //change to path after implementing
     }
 }
