6ed98d392728de212b26e188d83e9f847bacda57
Rafael Petouris Rodriguez de Paterna <R.PetourisRodriguezdePaterna@student.tudelft.nl>
2023-07-10 13:12:25 +0200

    Added pdf reading

--- SigPAC-Extractor/src/Classes/Extractor.java
+++ SigPAC-Extractor/src/Classes/Extractor.java
@@ -1,6 +1,14 @@
 package Classes;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 public class Extractor extends Shared {
 
@@ -29,8 +37,68 @@ public class Extractor extends Shared {
         System.out.println("¿Cómo quiere llamar a esta tabla?");
         String tableName = shared.getInputScanner().nextLine();
 
+        File file = new File("./Files/" + tableName + ".txt");
+
+        writeToFile(readPDF(pdfFileName), file);
+
         System.out.println("La tabla se ha guardado como \"" + tableName + "\""); //change to path after implementing
     }
 
 
+    private static String extract(String fileName) {
+        String text = "";
+
+        try {
+            File file = new File(fileName);
+            Scanner fileScanner = new Scanner(file);
+
+            while (fileScanner.hasNextLine()) {
+                text += fileScanner.nextLine() + "\n";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
+
+    private static String readPDF(String fileName) {
+
+        String text = "";
+
+        try (PDDocument document = PDDocument.load(new File(fileName))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+                PDFTextStripper tStripper = new PDFTextStripper();
+                String pdfFileInText = tStripper.getText(document);
+
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    text += line + "\n";
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
+
+    private static void writeToFile(String text, File file) {
+        try {
+            FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
+            fileWriter.write(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("There was an error creating the file.");
+        }
+    }
 }
